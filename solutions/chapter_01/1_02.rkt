57468f6e4591e96eac28d924b4f5e8220e528aa3
Andrew Romanov <me@andrew-r.ru>
2017-02-25 20:50:50 +0300

    Add solution for exercise 1_02

---
+++ solutions/chapter_01/1_02.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define solution (
+  /
+  (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
+  (* 3 (- 6 2) (- 2 7))
+))
+
+(module+ test
+  (require rackunit)
+
+  (test-case
+    "1_02"
+    (check-equal? solution -37/150)
+  )
+)
