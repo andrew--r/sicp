1f12be7a80f44cbd5843b420446b923c0c0be095
Andrew Romanov <me@andrew-r.ru>
2017-02-25 22:21:33 +0300

    Add descriptions for exercises 1_02 and 1_03

--- solutions/chapter_01/1_02.rkt
+++ solutions/chapter_01/1_02.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; Translate the following expression into prefix form: (see images/1_02.png)
+
 (define solution (
   /
   (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
--- solutions/chapter_01/1_03.rkt
+++ solutions/chapter_01/1_03.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
+
 (define (square x) (* x x))
 
 (module+ test
